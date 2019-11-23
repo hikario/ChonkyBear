3dca75a8188cfae4c8d7c5ed3d0119a0a783dd80
inann <xbox.inann@gmail.com>
2019-11-22 22:31:14 -0800

    Flipped shift to sprint, set Move Speed Multiplier to 4

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -13,7 +13,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
 
-        
+
         private void Start()
         {
             // get the transform of the main camera
@@ -62,9 +62,10 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                 // we use world-relative directions in the case of no main camera
                 m_Move = v*Vector3.forward + h*Vector3.right;
             }
+            m_Move = m_Move / 2f;
 #if !MOBILE_INPUT
 			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 2f;
 #endif
 
             // pass all parameters to the character control script
