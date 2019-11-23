06eaf8c0a19de37ae4a34a266ad4b26d62b02813
inann <xbox.inann@gmail.com>
2019-11-23 01:35:33 -0800

    Adding LuminescenceCollider as a prefab; drag and drop onto meshed objects for lights

---
+++ Assets/LuminescenceCollider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuminescenceCollider : MonoBehaviour {
+
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+
+  void OnTriggerEnter(Collider other) {
+    if (other.tag == "Player") {
+      Light lumi = gameObject.transform.parent.gameObject.GetComponentInChildren(typeof(Light)) as Light;
+
+      if (lumi != null) {
+        // Debug.Log("Something went right");
+        lumi.intensity = 4.0f;
+      } else {
+        Debug.Log("something went wrong");
+      }
+    }
+  }
+
+  void OnTriggerExit(Collider other) {
+    if (other.tag == "Player") {
+      Light lumi = gameObject.transform.parent.gameObject.GetComponentInChildren(typeof(Light)) as Light;
+
+      if (lumi != null) {
+        // Debug.Log("Something went right");
+        lumi.intensity = 0.0f;
+      } else {
+        Debug.Log("something went wrong");
+      }
+    }
+  }
+}
