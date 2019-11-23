cfb10a8d4c73e4bea8bb90d7b8183d517d90ed09
inann <xbox.inann@gmail.com>
2019-11-23 01:48:23 -0800

    Already improved; caches the light object instead of looking it up upon entering or exiting

--- Assets/LuminescenceCollider.cs
+++ Assets/LuminescenceCollider.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class LuminescenceCollider : MonoBehaviour {
 
+private Light lumi;
   // Use this for initialization
   void Start () {
-
+    lumi = gameObject.transform.parent.gameObject.GetComponentInChildren(typeof(Light)) as Light;
   }
 
   // Update is called once per frame
@@ -16,8 +17,6 @@ public class LuminescenceCollider : MonoBehaviour {
 
   void OnTriggerEnter(Collider other) {
     if (other.tag == "Player") {
-      Light lumi = gameObject.transform.parent.gameObject.GetComponentInChildren(typeof(Light)) as Light;
-
       if (lumi != null) {
         // Debug.Log("Something went right");
         lumi.intensity = 4.0f;
@@ -29,8 +28,6 @@ public class LuminescenceCollider : MonoBehaviour {
 
   void OnTriggerExit(Collider other) {
     if (other.tag == "Player") {
-      Light lumi = gameObject.transform.parent.gameObject.GetComponentInChildren(typeof(Light)) as Light;
-
       if (lumi != null) {
         // Debug.Log("Something went right");
         lumi.intensity = 0.0f;
