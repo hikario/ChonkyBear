4522e463498fead908ceab58e95304a333545032
inann <inann231@gmail.com>
2019-12-18 23:33:39 -0800

    HatSimulator 2019

---
+++ Assets/HiddenCollectible.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenCollectible : MonoBehaviour
+{
+
+  public GameObject hatStatus;
+  private Camera mc;
+
+  void Start() {
+    hatStatus = GameObject.FindGameObjectWithTag("HatState");
+    mc = Camera.main;
+  }
+
+  void OnTriggerEnter(Collider other) {
+    if (other.tag == "Player") {
+      if (gameObject.tag == "CollHat") {
+        hatStatus.GetComponent<GameHatState>().hatActive = true;
+      } else if (gameObject.tag == "CollStraw") {
+        hatStatus.GetComponent<GameHatState>().strawberryActive = true;
+      }
+
+      mc.SendMessage("UpdateHats");
+
+      Destroy(gameObject);
+    }
+  }
+
+}
